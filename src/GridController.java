a6c403e49c6d7f592366bf929e3a0492cee83469
manyiw99 <1762019598@qq.com>
2022-10-30 18:40:09 +0100

    change arraylist to 2d array

--- src/GridController.java
+++ src/GridController.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class GridController {
     String gridName;
-    ArrayList<Grid> grids;
+    Grid grids[][];
     int sunkNum;
     int NUM=10;
 
@@ -11,12 +11,11 @@ public class GridController {
         this.gridName=gridName;
         sunkNum=0;
 
-        grids = new ArrayList<Grid>();
-
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
+        grids = new Grid[10][10];
+        for (int i = 0; i < grids.length; i++) {
+            for (int j = 0; j < grids[0].length; j++) {
                 int[] loc={i,j};
-                grids.add(new Grid(loc,null,null,false));
+                grids[i][j] = new Grid(loc,null,null,false);
             }
         }
     }
@@ -25,67 +24,35 @@ public class GridController {
         System.out.println("===" + gridName + "===");
         System.out.println("A---------------J");
 
-        //补充标题之类 --> 将页面显示完整 (隐藏逻辑)
-
-        //可以访问每个grid的loc与Flag,画图可以转成二维数组，也可以直接用getLoc()与getFlag()获得每个格子的信息来画
-
         FLAGS[][] flags = new FLAGS[10][10];
-
-        // Construct 2-dimensional array
-        for (int i = 0; i < grids.size(); i++) {
-            int[] loc = grids.get(i).getLoc();
-            flags[loc[0]][loc[1]] = grids.get(i).getFlag();
+        for (int i = 0; i < grids.length; i++) {
+            for (int j = 0; j < grids[0].length; j++) {
+                flags[i][j] = grids[i][j].getFlag();
+            }
         }
         System.out.println(Arrays.deepToString(flags).replace("], ", "]\n"));
+
+        //补充标题之类 --> 将页面显示完整 (隐藏逻辑)
+        //最开始初始化的时候显示空网格
     }
 
     public void setGrid(int[] loc, FLAGS f, String name, boolean guess){
         //System.out.println("In gridController class:"+ Arrays.toString(loc) +" "+f +" name: "+name); //ok
-        for(int i = 0;i < grids.size(); i++){
-            if( Arrays.equals(grids.get(i).getLoc(),loc)){
-                grids.get(i).setFlag(f);
-                grids.get(i).setName(name);
-                grids.get(i).setGuess(guess);
-                break;
-            }
-        }
-
+        grids[loc[0]][loc[1]].setFlag(f);
+        grids[loc[0]][loc[1]].setName(name);
+        grids[loc[0]][loc[1]].setGuess(guess);
     }
 
     public FLAGS retrieveFlag(int[] loc){
-        FLAGS result = null;
-        for(int i = 0;i < grids.size(); i++){
-            if( Arrays.equals(grids.get(i).getLoc(),loc)){
-                result = grids.get(i).getFlag();
-                break;
-            }
-        }
-        
-        return result;
+        return grids[loc[0]][loc[1]].getFlag();
     }
 
     public String retrieveName(int[] loc){
-        String result = null;
-        for(int i = 0;i < grids.size(); i++){
-            if( Arrays.equals(grids.get(i).getLoc(),loc)){
-                result = grids.get(i).getName();
-                break;
-            }
-        }
-
-        return result;
+        return grids[loc[0]][loc[1]].getName();
     }
 
     public boolean retrieveGuess(int[] loc){
-        boolean result = false;
-        for(int i = 0;i < grids.size(); i++){
-            if( Arrays.equals(grids.get(i).getLoc(),loc)){
-                result = grids.get(i).getGuess();
-                break;
-            }
-        }
-
-        return result;
+        return grids[loc[0]][loc[1]].getGuess();
     }
 
 
@@ -106,15 +73,16 @@ public class GridController {
     public boolean isSunk(String name){
         boolean result=false;
 
-        for(int i = 0;i < grids.size(); i++){
-            if( grids.get(i).getName() == name){
-                if (grids.get(i).getFlag() != FLAGS.X) {
-                    result = false;
-                    break;
-                }
-                else{
-                    result=true;
+        for(int i = 0;i < grids.length; i++){
+            for (int j = 0; j < grids[0].length; j++) {
+                if (grids[i][j].getName() == name) {
+                    if (grids[i][j].getFlag() != FLAGS.X) {
+                        result = false;
+                        break;
+                    } else {
+                        result = true;
 
+                    }
                 }
             }
         }
@@ -131,9 +99,11 @@ public class GridController {
             }
         }
 
-        for(int i = 0;i < grids.size(); i++){
-            if( grids.get(i).getName() == name){
-                grids.get(i).setFlag(originalFlag);
+        for(int i = 0;i < grids.length; i++){
+            for (int j = 0; j < grids[0].length; j++) {
+                if (grids[i][j].getName() == name) {
+                    grids[i][j].setFlag(originalFlag);
+                }
             }
         }
 
